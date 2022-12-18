a6325a7ffbf3e6891a17716271a3b4f8b949ad96
Philippe Lécaillon <lecaillon@hotmail.com>
2022-12-18 01:36:41 +0100

    Clean code

--- frontend/src/components/FormSearchSale.vue
+++ frontend/src/components/FormSearchSale.vue
@@ -16,60 +16,11 @@ async function searchCommunes(val: any) {
   <VCard variant="flat">
     <v-card-text>
       <v-container>
-        <v-autocomplete
-          label="Ville"
-          v-model="store.searchQuery.commune"
-          :items="communes"
-          item-value="code"
-          item-title="nom"
-          @update:search="searchCommunes"
-          return-object
-          no-filter
-          hide-no-data
-          clearable
-          density="comfortable"
-          color="secondary"
-        ></v-autocomplete>
-        <v-range-slider
-          v-model="store.searchQuery.budgetRange"
-          label="Budget"
-          step="100000"
-          min="0"
-          max="2000000"
-          thumb-label="always"
-          class="mt-8"
-          color="secondary"
-        ></v-range-slider>
-        <v-range-slider
-          v-model="store.searchQuery.batiRange"
-          label="Surface bati"
-          step="10"
-          min="0"
-          max="300"
-          thumb-label="always"
-          class="mt-8"
-          color="secondary"
-        ></v-range-slider>
-        <v-range-slider
-          v-model="store.searchQuery.terrainRange"
-          label="Surface terrain"
-          step="100"
-          min="0"
-          max="10000"
-          thumb-label="always"
-          class="mt-8"
-          color="secondary"
-        ></v-range-slider>
-        <v-range-slider
-          v-model="store.searchQuery.anneeRange"
-          label="Année"
-          step="1"
-          min="2017"
-          max="2022"
-          thumb-label="always"
-          class="mt-8"
-          color="secondary"
-        ></v-range-slider>
+        <v-autocomplete label="Ville" v-model="store.searchQuery.commune" :items="communes" item-value="code" item-title="nom" @update:search="searchCommunes" return-object no-filter hide-no-data clearable density="comfortable" color="secondary"></v-autocomplete>
+        <v-range-slider label="Budget" v-model="store.searchQuery.budgetRange" step="100000" min="0" max="2000000" thumb-label="always" class="mt-8" color="secondary"></v-range-slider>
+        <v-range-slider label="Surface bati" v-model="store.searchQuery.batiRange" step="10" min="0" max="300" thumb-label="always" class="mt-8" color="secondary"></v-range-slider>
+        <v-range-slider label="Surface terrain" v-model="store.searchQuery.terrainRange" step="100" min="0" max="10000" thumb-label="always" class="mt-8" color="secondary"></v-range-slider>
+        <v-range-slider label="Année" v-model="store.searchQuery.anneeRange" step="1" min="2017" max="2022" thumb-label="always" class="mt-8" color="secondary"></v-range-slider>
       </v-container>
     </v-card-text>
     <v-card-actions>
